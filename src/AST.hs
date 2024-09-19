5eef698a5de08c1a6541b84d6bb4616b9b6c3f0c
Guillaume MICHEL <guillaume.michel@epitech.eu>
2024-09-19 21:44:42 +0200

    ADD BangPatterns and strict evaluation to optimise time execution and memory usage

--- src/AST.hs
+++ src/AST.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 module AST (SExpr(..), Ast(..), sexprToAST) where
 
 data SExpr
@@ -26,38 +28,32 @@ sexprToAST (SInt n) = Right (AstInt n)
 sexprToAST (SBool b) = Right (AstBool b)
 sexprToAST (SSymbol s) = Right (AstSym s)
 
-sexprToAST (SList [SSymbol "define", SSymbol var, expr]) = do
-    astExpr <- sexprToAST expr
-    Right (Define var astExpr)
+sexprToAST (SList [SSymbol "define", SSymbol var, expr]) = 
+    Define var <$> sexprToAST expr
 
 sexprToAST (SList [SSymbol "define", SList (SSymbol funcname : params), body]) = do
-    paramNames <- mapM symbolToString params
-    bodyAst <- sexprToAST body
-    Right (Define funcname (Lambda paramNames bodyAst))
+    !paramNames <- mapM symbolToString params
+    !bodyAst <- sexprToAST body
+    return $ Define funcname (Lambda paramNames bodyAst)
 
-sexprToAST (SList [SSymbol "if", cond, thenExpr, elseExpr]) = do
-    condAst <- sexprToAST cond
-    thenAst <- sexprToAST thenExpr
-    elseAst <- sexprToAST elseExpr
-    Right (If condAst thenAst elseAst)
+sexprToAST (SList [SSymbol "if", cond, thenExpr, elseExpr]) = 
+    If <$> sexprToAST cond <*> sexprToAST thenExpr <*> sexprToAST elseExpr
 
 sexprToAST (SList [SSymbol "lambda", SList params, body]) = do
-    paramNames <- mapM symbolToString params
-    bodyAst <- sexprToAST body
-    Right (Lambda paramNames bodyAst)
+    !paramNames <- mapM symbolToString params
+    !bodyAst <- sexprToAST body
+    return $ Lambda paramNames bodyAst
 
-sexprToAST (SList (SSymbol func : args)) = do
-    astArgs <- mapM sexprToAST args
-    Right (Call func astArgs)
+sexprToAST (SList (SSymbol func : args)) = 
+    Call func <$> mapM sexprToAST args
 
 sexprToAST (SList (lambdaExpr : args)) = do
-    funcAst <- sexprToAST lambdaExpr
-    argAsts <- mapM sexprToAST args
-    Right (CallLambda funcAst argAsts)
+    !funcAst <- sexprToAST lambdaExpr
+    !argAsts <- mapM sexprToAST args
+    return $ CallLambda funcAst argAsts
 
-sexprToAST (SList exprs) = do
-    astExprs <- mapM sexprToAST exprs
-    Right (AstList astExprs)
+sexprToAST (SList exprs) = 
+    AstList <$> mapM sexprToAST exprs
 
 symbolToString :: SExpr -> Either String String
 symbolToString (SSymbol s) = Right s
