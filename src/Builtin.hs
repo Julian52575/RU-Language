f48f64de2ebad6414908c4c415e0adb9d9428eed
Guillaume MICHEL <guillaume.michel@epitech.eu>
2024-09-19 21:43:29 +0200

    ADD BangPatterns and strict evaluation to optimise time execution and memory usage

--- src/Builtin.hs
+++ src/Builtin.hs
@@ -1,11 +1,12 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Builtin (evalBuiltinFunction) where
 
 import AST (Ast(..))
+import Data.List (foldl')
 
--- Type alias for built-in functions, taking a list of ASTs and returning either an error message or an AST
 type BuiltinFunction = [Ast] -> Either String Ast
 
--- Table of built-in functions with first-class functions
 builtinFunctions :: [(String, BuiltinFunction)]
 builtinFunctions =
     [ ("+", evalAdd)
@@ -20,44 +21,49 @@ builtinFunctions =
     , ("eq?", evalEq)
     ]
 
--- Function to look up and execute a built-in function
 evalBuiltinFunction :: String -> [Ast] -> Either String Ast
-evalBuiltinFunction func args = maybe (Left $ "Unknown function: " ++ func) ($ args) (lookup func builtinFunctions)
--- Uses `maybe` for more concise lookup and function execution
+evalBuiltinFunction func args = maybe (Left $ "Unknown function: " ++ func) (\f -> f args) (lookup func builtinFunctions)
 
--- Arithmetic and comparison built-in function implementations
+-- Helper function to extract an integer from an AST node
+getInt :: Ast -> Either String Int
+getInt (AstInt n) = Right n
+getInt _ = Left "Expected an integer"
+
+-- Arithmetic functions
 evalAdd, evalSub, evalMul, evalDiv, evalMod :: BuiltinFunction
 
 -- Addition
 evalAdd [] = Left "Addition requires at least one argument"
-evalAdd args = AstInt . sum <$> mapM getInt args
--- Uses `<$>` to simplify the function composition and avoid explicit `do` block
+evalAdd args = do
+    !intArgs <- mapM getInt args
+    return $ AstInt (sum intArgs)
 
 -- Subtraction
 evalSub [] = Left "Subtraction requires at least one argument"
 evalSub (x:xs) = do
-    intX <- getInt x
-    intXs <- mapM getInt xs
-    return $ AstInt (foldl (-) intX intXs)
+    !intX <- getInt x
+    !intXs <- mapM getInt xs
+    return $ AstInt (foldl' (-) intX intXs)
 
 -- Multiplication
 evalMul [] = Left "Multiplication requires at least one argument"
-evalMul args = AstInt . product <$> mapM getInt args
--- Similar optimization with `<$>` for readability
+evalMul args = do
+    !intArgs <- mapM getInt args
+    return $ AstInt (product intArgs)
 
 -- Division
 evalDiv [] = Left "Division requires at least two arguments"
 evalDiv (x:xs) = do
-    intX <- getInt x
-    intXs <- mapM getInt xs
+    !intX <- getInt x
+    !intXs <- mapM getInt xs
     if 0 `elem` intXs
         then Left "Division by zero error"
-        else return $ AstInt (foldl div intX intXs)
+        else return $ AstInt (foldl' div intX intXs)
 
 -- Modulo
 evalMod [x, y] = do
-    intX <- getInt x
-    intY <- getInt y
+    !intX <- getInt x
+    !intY <- getInt y
     if intY == 0
         then Left "Modulo by zero error"
         else return $ AstInt (intX `mod` intY)
@@ -66,42 +72,37 @@ evalMod _ = Left "Modulo requires exactly two arguments"
 -- Comparison functions
 evalLt, evalGt, evalLe, evalGe, evalEq :: BuiltinFunction
 
--- Less than comparison
+-- Less than
 evalLt [x, y] = do
-    intX <- getInt x
-    intY <- getInt y
+    !intX <- getInt x
+    !intY <- getInt y
     return $ AstBool (intX < intY)
 evalLt _ = Left "Less than requires two arguments"
 
--- Greater than comparison
+-- Greater than
 evalGt [x, y] = do
-    intX <- getInt x
-    intY <- getInt y
+    !intX <- getInt x
+    !intY <- getInt y
     return $ AstBool (intX > intY)
 evalGt _ = Left "Greater than requires two arguments"
 
--- Less than or equal comparison
+-- Less than or equal to
 evalLe [x, y] = do
-    intX <- getInt x
-    intY <- getInt y
+    !intX <- getInt x
+    !intY <- getInt y
     return $ AstBool (intX <= intY)
 evalLe _ = Left "Less than or equal to requires two arguments"
 
--- Greater than or equal comparison
+-- Greater than or equal to
 evalGe [x, y] = do
-    intX <- getInt x
-    intY <- getInt y
+    !intX <- getInt x
+    !intY <- getInt y
     return $ AstBool (intX >= intY)
 evalGe _ = Left "Greater than or equal to requires two arguments"
 
--- Equality comparison
+-- Equality check
 evalEq [x, y] = do
-    intX <- getInt x
-    intY <- getInt y
+    !intX <- getInt x
+    !intY <- getInt y
     return $ AstBool (intX == intY)
 evalEq _ = Left "Equality check requires two arguments"
-
--- Helper function to extract an integer from an AST node
-getInt :: Ast -> Either String Int
-getInt (AstInt n) = Right n
-getInt _ = Left "Expected an integer"
