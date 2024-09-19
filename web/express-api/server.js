02368c2c7e85da80b50e2d93bd294c8ea1fa3d99
neo <neo.burtin@epitech.eu>
2024-09-19 12:58:44 +0200

    [ADD] angular and express api

---
+++ web/express-api/server.js
@@ -0,0 +1,13 @@
+// express-api/server.js
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express API!');
+});
+
+app.listen(port, () => {
+  console.log(`Express API running on http://localhost:${port}`);
+});
