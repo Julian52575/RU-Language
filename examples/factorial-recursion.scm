80e487c50608f02a5bd4f7928afe4a86c8776cbc
julian <julian.bottiglione@epitech.eu>
2024-09-25 16:40:41 +0200

    [fix] Using our syntax

--- examples/factorial-recursion.scm
+++ examples/factorial-recursion.scm
@@ -1,11 +1,6 @@
-; factorial.scm
 (define (factorial n)
   (if (<= n 1)
       1
       (* n (factorial (- n 1)))))
 
-; Test cases
-(display (factorial 5)) ; 120
-(newline)
-(display (factorial 0)) ; 1
-(newline)
+(factorial 5)
