dd435c2ead96521cad4dcbf56829b14704db7c61
julian <julian.bottiglione@epitech.eu>
2024-11-02 18:46:36 +0100

    [add] Printing success in green and checking reference file exists

--- examples/tests_run.sh
+++ examples/tests_run.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
-EXAMPLES=("multiplePrint")
+EXAMPLES=("multiplePrint" "fizzbuzz")
 FAILURES=""
 SUCCESS=""
 LOG_FILE=""
 
 RED='\033[0;31m'
+GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 stack build
 for current in ${EXAMPLES[@]}; do
+    if ! [ -f "examples/$current.txt" ]
+    then
+        FAILURES=$FAILURES"\texamples/$current.txt not found."
+        continue
+    fi
+
     LOG_FILE="examples/log/$current.log"
     stack run "--" "examples/$current.bin" > $LOG_FILE
     ## diff -> FAILURE
     diff $LOG_FILE "examples/$current.txt" > /dev/null
     if [ $? -ne 0 ]
     then
-        FAILURE=$FAILURE"\t$current: output differs.\n"
+        FAILURES=$FAILURES"\t$current: output differs.\n"
     else
         SUCCESS=$SUCCESS"\t$current: output matches.\n"
     fi
 done
 
-echo -e "Succes:\n$SUCCESS"
-echo -e "Failures:$RED$FAILURE$NC"
+echo -e "Succes:\n$GREEN $SUCCESS $NC"
+echo -e "Failures:\n$RED $FAILURES $NC"
